544425f2c40d5e3cafc5d695a9ce816ac8c05f97
Rex Chan <Rex@Rexs-MacBook-Pro-2.local>
2017-11-03 16:33:08 +0800

    #2: Hello Reply Trial #18, trying to use .*

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -304,7 +304,7 @@ public class KitchenSinkController {
 		}*/
 
 		default:{
-			if ((text.toLowerCase().matches("hi(.*)")) or (text.toLowerCase().matches("hello(.*)")))
+			if ((text.toLowerCase().matches("hi(.*)")))
 			{
 				String userId = event.getSource().getUserId();
 				if (userId != null) {
