de3ba45d5bd55393ad8b6fa652af5c8e7df5b7c7
ryanthh2012 <32058750+ryanthh2012@users.noreply.github.com>
2017-11-03 23:14:59 +0800

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -288,18 +288,7 @@ public class KitchenSinkController {
             		}            	
             	this.reply(replyToken, multiMessages);
             	break;
-            }
-            case "hello": {
-                String userId = event.getSource().getUserId();
-                if (userId != null) {
-                    lineMessagingClient
-                            .getProfile(userId)
-                            .whenComplete(new greetWithName (this, replyToken));
-                } else {
-                    this.replyText(replyToken, "Bot can't use profile API without user ID");
-                }
-                break;
-            }            
+            }      
             default:
             	String reply = null;
             	try {
@@ -407,28 +396,4 @@ public class KitchenSinkController {
     	}
     }
 	
-	// Greeting the user with their Display Name (Ryan Tang)
-	class greetWithName implements BiConsumer<UserProfileResponse, Throwable> {
-		private KitchenSinkController ksc;
-		private String replyToken;
-		
-		public greetWithName(KitchenSinkController ksc, String replyToken) {
-			this.ksc = ksc;
-			this.replyToken = replyToken;
-		}
-		@Override
-    	public void accept(UserProfileResponse profile, Throwable throwable) {
-    		if (throwable != null) {
-            	ksc.replyText(replyToken, throwable.getMessage());
-            	return;
-        	}
-        	ksc.reply(
-                	replyToken,
-                	Arrays.asList(new TextMessage(
-                		"Hello, " + profile.getDisplayName() + "!  How may I help you?"))
-        	);
-    	}
-	}
-	
-
 }
